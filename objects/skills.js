04cc24cf61528569e03d32fc98fc1e6c72aec450
Glazelf <gideon.steenbergen@gmail.com>
2023-09-26 21:08:25 +0200

    add more skills/talents/traits

--- objects/skills.js
+++ objects/skills.js
@@ -1,3 +1,72 @@
-let skills {
-
+let skills = {
+    chilly_breath: {
+        name: "Chilly Breath",
+        type: "Attack",
+        description: "Inflicts medium ice-elemental breath damage on all enemies."
+    },
+    cool_breath: {
+        name: "Cool Breath",
+        type: "Attack",
+        description: "Inflicts minor ice-elemental breath damage on all enemies."
+    },
+    dazzle: {
+        name: "Dazzle",
+        type: "Status",
+        description: "Bedazzles all enemies for 2 to 5 turns. The higher the caster's wisdom, the more likely the spell is to succeed."
+    },
+    deep_breath: {
+        name: "Deep Breath",
+        type: "Status",
+        description: "Inhales long and deep, raising the damage inflicted by the user's next breath attack."
+    },
+    drain_magic: {
+        name: "Drain Magic",
+        type: "Status",
+        description: "Absorbs some MP from a single enemy."
+    },
+    fire_breath: {
+        name: "Fire Breath",
+        type: "Attack",
+        description: "Inflicts minor fire-elemental breath damage on all enemies."
+    },
+    fizzle: {
+        name: "Fizzle",
+        type: "Status",
+        description: "Prevents all enemies from casting spells for 4 to 7 turns. The higher the caster's wisdom, the more likely the spell is to succeed."
+    },
+    flame_breath: {
+        name: "Flame Breath",
+        type: "Attack",
+        descriptionn: "Inflicts medium fire-elemental breath damage on all enemies."
+    },
+    heal: {
+        name: "Heal",
+        type: "Healing",
+        description: "Restores 28 or more HP to a single ally."
+    },
+    miracle_slash: {
+        name: "Miracle Slash",
+        type: "Attack",
+        description: "Inflicts slashing damage on a single enemy. Approximately half the amount of damage inflicted is restored to the user as HP. Can heal up to a maximum of 200 HP."
+    },
+    sandstorm: {
+        name: "Sandstorm",
+        type: "Status",
+        description: "Whips up a dense cloud of dust that lowers the accuracy of enemy attacks for 2 to 5 turns."
+    },
+    sleep_attack: {
+        name: "Sleep Attack",
+        type: "Attack",
+        description: "Damages a single enemy and occasionally puts it to sleep for 2 to 5 turns."
+    },
+    snooze: {
+        name: "Snooze",
+        type: "Status",
+        description: "Puts a single enemy to sleep for 2 to 5 turns. The higher the caster's wisdom, the more likely the spell is to succeed."
+    },
+    squelch: {
+        name: "Squelch",
+        type: "Healing",
+        description: "Cures all allies of poison or severe poison."
+    }
 };
