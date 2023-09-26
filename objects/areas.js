643922223e0023d913a6fd80238557af11b39b68
Glazelf <gideon.steenbergen@gmail.com>
2023-09-26 18:41:29 +0200

    add first 3 areas

--- objects/areas.js
+++ objects/areas.js
@@ -1,3 +1,5 @@
 let areas = {
-
+    terrestria: ["slime", "wing_slime", "dragonthorn", "komodo", "vegandragora", "bantamweight", "killerpillar", "capsichum", "woebergine", "khalamari_kid", "antguzzler", "mischievous_mole", "bunicorn", "feralball", "dracky", "teeny_sanguini", "gigantes", "stump_chump"],
+    circle_of_conquest: ["wing_slime", "bubble_slime", "damselfly", "lumbering_lizard", "mad_mollusk", "woebergine", "antguzzler", "feralball", "leery_lout", "lunatick", "lips", "putrefido", "funghoul", "skeleton", "ghost"],
+    circle_of_temper: ["healslime", "she_slime", 68, 87, "stark_raven", "toady", "cyclown", "mischievous_mole", "lampling", "foo_dog", "bongo_drongo", "leery_lout", "dracky", "lunatick", "firespirit", 441]
 };
