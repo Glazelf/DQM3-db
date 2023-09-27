649a18434124d9e63619c9a38d7cbf77a02acbb3
Glazelf <gideon.steenbergen@gmail.com>
2023-09-27 19:12:01 +0200

    build out function for generic synth

--- util/synthesis.js
+++ util/synthesis.js
@@ -1,14 +1,28 @@
-module.exports = (parent1, parent2) => {
-    let synthesisResults = [];
-    const genericSynthesis = require('../objects/genericSynthesis.json');
-    let parentsSameRank = (parent1.rank === parent2.rank);
+module.exports = (parent1, parent2, target) => {
+    // Behaviour:
+    // 1. parent1 & parent 2 --> return results
+    // 2. 1 parent --> return options for synthesis with that parent
+    // 3. 1 parent & target --> return options (or path) to get from 1 parent to target
+    // 4. target --> return options to synthesize target
+    // 5. All 3 --> check if parents synthesize into target
 
-    for (let key in genericSynthesis) {
-        let keySplit = key.split("_");
-        if (keySplit.has(parent1.family) && keySplit.has(parent2.family)) {
-            synthesisResults.push(genericSynthesis[key][parent1.rank])
-            if (!parentsSameRank) synthesisResults.push(genericSynthesis[key][parent2.rank])
+    const monsters = require('../objects/monsters.json');
+    const genericSynthesis = require('../objects/genericSynthesis.json');
+
+    let parent1data = monsters[parent1];
+    let parent2data = monsters[parent2];
+    let parentsSameRank = (parent1data.rank === parent2data.rank);
+    // Behaviour 1
+    if (parent1 && parent2) {
+        let synthesisResults = [];
+        for (let key in genericSynthesis) {
+            let keySplit = key.split("_");
+            if (keySplit.includes(parent1data.family) && keySplit.includes(parent2data.family)) {
+                synthesisResults = synthesisResults.concat(genericSynthesis[key][parent1data.rank])
+                if (!parentsSameRank) synthesisResults = synthesisResults.concat(genericSynthesis[key][parent2data.rank])
+            };
         };
+        synthesisResults = synthesisResults.concat([parent1, parent2]);
+        return synthesisResults;
     };
-    return synthesisResults;
 };
