1d28cf3820d4ad2d43d54dfb6e02221a5bab77b6
Glazelf <gideon.steenbergen@gmail.com>
2023-09-27 20:11:45 +0200

    move tests

---
+++ tests/synthesisTests.js
@@ -0,0 +1,8 @@
+const synthesis = require('../util/synthesis');
+const monsters = require('../objects/monsters.json');
+let parent1 = "drake_slime";
+let parent2 = "onion_slime";
+let target = null;
+let bothParentsResult = synthesis(parent1, parent2);
+console.log(`parent1: ${parent1} + parent2: ${parent2}:`);
+console.log(bothParentsResult);
